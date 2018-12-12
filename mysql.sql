ac20f708aedccb3f298a52270baa49b19105e3bd
razerkin <35743059+razerkin@users.noreply.github.com>
2018-12-12 19:56:34 +0800

    The sql file to create database and usertable

---
+++ mysql.sql
@@ -0,0 +1,19 @@
+create database openvpn;
+use openvpn;
+create table vpnuser (
+   name         char (100) not null,
+   password     char (255) default null,
+   active       int (10) not null default 1,
+   primary key  (name)
+   );
+create table logtable (
+   msg     char (254),
+   user    char (100),
+   pid     char (100),
+   host    char (100),
+   rhost   char (100),
+   time    char (100)
+   );
+insert into vpnuser (name,password) values ('test1',password('test1'));
+grant all on openvpn.* to vpnadmin@'localhost' identified by 'decli';
+flush privileges;
